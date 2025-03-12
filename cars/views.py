9e8ae5355d511b8646a9e6e737d07891b76f98fd
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-03-12 11:55:09 -0300

    Aprendendo a passar os primeiros parâmetros para página e formatando os números Type(float, int)

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+import locale
 
-def cars_views( request ):
-    return render( request, 'cars/cars_index.html' )
+# Configura a localidade para o formato brasileiro
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+def format_float(value):
+    return locale.format_string('%.2f', value, grouping=True)
+
+def format_int(value):
+    return locale.format_string('%.f', value, grouping=True)
+
+# Create your views here.
+def cars_views(request):
+    car = {
+        'model': 'Maverick',
+        'id': format_int(1579),
+        'description': 'Carro Esportivo - Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        'value': format_float(19854758.89),
+    }
+    return render(request, 'cars/cars_index.html', {'cars': car})
