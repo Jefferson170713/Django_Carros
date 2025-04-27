4a25ae15e041620745e4b42431ea0bdc66017063
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-04-27 15:32:50 -0300

    dev - adicionando a urls ao projeto

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_views
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cars.urls')),
+    path('register/', include('accounts.urls')),
+    #path('cars/', include('cars.urls')),
+    path('', include('cars.urls')), # aqui é minha raiz do projeto, então não preciso colocar /cars/ na url
 ] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
