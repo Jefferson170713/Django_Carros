69a9bfe54e738fedea797cc12ff70de4b60244ec
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-04-27 15:33:27 -0300

    dev - criando a viewa de accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import UserCreationForm
 
-# Create your views here.
+def register_view(request):
+    user_form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'user_form': user_form})
