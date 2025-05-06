577a42deea72ea27cebf9781fc3301093129a457
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-05-06 15:12:40 -0300

    dev - 061 Atualizando o registro.

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars_views
-from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     #path('cars/', include('cars.urls')),
-    path('', include('cars.urls')), # aqui é minha raiz do projeto, então não preciso colocar /cars/ na url
+    path('', include('cars.urls')), 
 ] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
