090d9fe602f18d95870e6958eb0db52fd0ef5d23
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-05-26 17:06:07 -0300

    dev - Criação das views de login e logout da aplicação com permissão de usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -11,4 +15,22 @@ def register_view(request):
     else:
         user_form = UserCreationForm()
     return render(request, 'accounts/register.html', {'user_form': user_form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('cars:cars_index')
+    else:
+        login_form = AuthenticationForm()
+    return render( request, 'accounts/login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
 
