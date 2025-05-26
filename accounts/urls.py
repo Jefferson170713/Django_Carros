2882fa31eca56c98e6c287acbbf0bb2c89076793
jefferson Django <jefferson.almeida05@aluno.ifce.edu.br>
2025-05-26 17:07:19 -0300

    dev - criação das rotas de login e logout da aplicação

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from accounts.views import register_view
+from accounts.views import login_view
+from accounts.views import logout_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
